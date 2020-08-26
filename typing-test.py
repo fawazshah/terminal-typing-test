f0864c14f644c1d33dd6780a6ebd80b0cca457a7
Fawaz Shah <fawaz010@gmail.com>
2020-08-26 13:33:15 +0100

    Add green background when typing

--- typing-test.py
+++ typing-test.py
@@ -5,9 +5,17 @@ import sys
 ESCAPE = 27
 BACKSPACE = 127
 
+TEST_STRING = "Type these words"
+TEST_STRING_LENGTH = len(TEST_STRING)
+
 def main(stdscr):
     stdscr.addstr(0, 0, "Welcome to Typing Test! Press Ctrl-C to quit.")
-    stdscr.move(1, 0)
+    stdscr.addstr(2, 0, TEST_STRING)
+    stdscr.move(2, 0)
+
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
+
+    current_char = 0
 
     while True:
         try:
@@ -20,10 +28,17 @@ def main(stdscr):
         elif key == BACKSPACE:
             y, x = stdscr.getyx()
             if x > 0:
-                stdscr.delch(y, x-1)
+                stdscr.delch(y, x - 1)
         else:
             y, x = stdscr.getyx()
-            stdscr.addstr(y, x, chr(key))
+
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(y, x, TEST_STRING[current_char])
+            stdscr.attroff(curses.color_pair(1))
+
+
+            current_char += 1
+            stdscr.move(2, current_char)
 
 
 curses.wrapper(main)
