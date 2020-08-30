1906bc8ad248e9a315ef2cd6e10915b585654825
Fawaz Shah <fawaz010@gmail.com>
2020-08-30 01:50:56 +0100

    Add wpm calculation and prnt at end

--- typing-test.py
+++ typing-test.py
@@ -2,14 +2,32 @@ import curses
 import time
 import sys
 
+ENTER_KEYS = {10, 13}
 ESCAPE = 27
-BACKSPACE = 127
+BACKSPACE_KEYS = {8, 127}
 
 CORRECT_COLOUR = 1
 ERROR_COLOUR = 2
 
 TEST_STRING = "Type these words"
 TEST_STRING_LENGTH = len(TEST_STRING)
+
+SCORE = 0
+
+
+def print_char(scr, char, colour_profile):
+    scr.attron(curses.color_pair(colour_profile))
+    y, x = scr.getyx()
+    scr.addstr(y, x, char)
+    scr.attroff(curses.color_pair(colour_profile))
+
+
+def wait_for_enter_pressed(scr):
+    scr.nodelay(0)
+    key = None
+    while key not in ENTER_KEYS:
+        key = scr.getch()
+    return
 
 
 def main(stdscr):
@@ -22,6 +40,10 @@ def main(stdscr):
 
     curr_char_idx = 0
 
+    num_words_typed = 1
+
+    start_time = time.time()
+
     while True:
         try:
             key = stdscr.getch()
@@ -30,23 +52,27 @@ def main(stdscr):
 
         if key == ESCAPE:
             break
-        elif key == BACKSPACE:
+        elif key in BACKSPACE_KEYS:
             y, x = stdscr.getyx()
             if x > 0:
                 stdscr.delch(y, x - 1)
         else:
-            if curr_char_idx >= TEST_STRING_LENGTH - 1:
-                sys.exit()
-
-            colour_profile = CORRECT_COLOUR if key == ord(TEST_STRING[curr_char_idx]) else ERROR_COLOUR
-
-            stdscr.attron(curses.color_pair(colour_profile))
-            y, x = stdscr.getyx()
-            stdscr.addstr(y, x, TEST_STRING[curr_char_idx])
-            stdscr.attroff(curses.color_pair(colour_profile))
-
-            curr_char_idx += 1
-            stdscr.move(2, curr_char_idx)
+            if curr_char_idx < TEST_STRING_LENGTH - 1:
+                if TEST_STRING[curr_char_idx] == " ":
+                    num_words_typed += 1
+                colour_profile = CORRECT_COLOUR if key == ord(TEST_STRING[curr_char_idx]) else ERROR_COLOUR
+                print_char(stdscr, TEST_STRING[curr_char_idx], colour_profile)
+                curr_char_idx += 1
+                stdscr.move(2, curr_char_idx)
+            else:
+                end_time = time.time()
+                time_taken = end_time - start_time
+                stdscr.clear()
+                stdscr.addstr(0, 0, f"Your time is {round(time_taken, 3)}.")
+                stdscr.addstr(1, 0, f"Your number of words typed is {num_words_typed}.")
+                stdscr.addstr(2, 0, f"Your WPM is {round(num_words_typed / time_taken * 60, 3)}. Press Enter to quit.")
+                wait_for_enter_pressed(stdscr)
+                break
 
 
 curses.wrapper(main)
